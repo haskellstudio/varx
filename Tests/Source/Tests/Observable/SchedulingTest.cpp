4e1370d9b29d507b59dea561673edd4ef5b55157
Martin Finke <martin@martin-finke.de>
2017-05-03 09:03:32 +0200

    Added empty SchedulingTest file

---
+++ Tests/Source/Tests/Observable/SchedulingTest.cpp
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    SchedulingTest.cpp
+    Created: 3 May 2017 8:26:03am
+    Author:  Martin Finke
+
+  ==============================================================================
+*/
+
+#include "rxjuce_TestPrefix.h"
+
+#include "rxjuce_Observable.h"
+#include "rxjuce_Observer.h"
+
+
+TEST_CASE("Observable::observeOn",
+		  "[Observable][Observable::observeOn]")
+{
+}
